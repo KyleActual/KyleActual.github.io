8f4322561fb5a8b2e986bbe890f92dfd9d9e3fe4
KyleActual <kylebarbehenn@gmail.com>
2025-04-11 20:44:31 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+function fetchGradeData() {
+console.log("fetching grade data...");
+}
+
+
+function populateGradebook(data) {
+
+
+
+console.log("populating gradebook with data:",data);
+}
+
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
